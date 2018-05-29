2020046a1361c6d9ddcb3f9937e237811c3c30de
Haleigh Dandridge <haleighdandridge@haleigh-dandridges-macbook-2.local>
2018-05-29 14:22:13 -0700

    fix

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var app = express()
 var mysql = require("mysql")
 var bodyParser = require("body-parser")
 
+var port = process.env.PORT || 3000
+
 app.get("/", function(req,res){
     res.send("The app is working")
 })
 
-app.listen(3000)
+app.listen(port)
